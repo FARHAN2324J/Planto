275fe7c68266301abd432443e369b474825ddeac
farhan <farhan@gmail.com>
2025-09-24 21:35:58 +0330

    nav

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -21,7 +21,7 @@ const NavbarMd = () => {
 
   return (
     <nav className="z-[9999] relative md:block hidden">
-      <ul className="flex fixed justify-between border-b-1 border-[#1b2316] backdrop-blur-xl bg-gradient-to-t  from-[#1b231600] to-[#1b2316] items-center w-full px-8 py-5 shadow-lg">
+      <ul className="flex fixed justify-between backdrop-blur-xl bg-gradient-to-t  from-[#1b231600] to-[#1b2316] items-center w-full px-8 py-5 shadow-lg">
         <li
           className="flex items-end gap-2 cursor-pointer "
           onClick={() => scrollToSection("Home-p")}
@@ -124,7 +124,7 @@ const NavbarSm = () => {
 
   return (
     <nav className="z-[9999] relative md:hidden block">
-      <ul className="flex fixed justify-between border-b-1 border-[#1b2316] shadow-lg backdrop-blur-xl bg-gradient-to-t from-[#1b231600] to-[#1b2316] items-baseline w-full p-5">
+      <ul className="flex fixed justify-between  shadow-lg backdrop-blur-xl bg-gradient-to-t from-[#1b231600] to-[#1b2316] items-baseline w-full p-5">
         <li
           className="flex items-end gap-2 cursor-pointer "
           onClick={() => handleMenuClick("Home-p")}
